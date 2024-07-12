de5ff06e090a0570ba756c1907864bd4b231f176
Alan Souza <alancesarsouza@gmail.com>
2024-07-12 11:55:58 -0300

    0.2.1

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,19 +1,19 @@
 local M = {}
 
 M.install = {
-  'numToStr/Comment.nvim',
-  requires = { 'JoosepAlviste/nvim-ts-context-commentstring' },
-  config = function()
-    require('Comment').setup {
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-    }
-
-    require('ts_context_commentstring').setup {
-      enable_autocmd = false,
-    }
-  end,
+  'JoosepAlviste/nvim-ts-context-commentstring',
+  requires = { 'numToStr/Comment.nvim' },
 }
 
-M.after = function() end
+M.after = function()
+  local status_ok, plugin = pcall(require, 'Comment')
+  if not status_ok then
+    return
+  end
+
+  plugin.setup {
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  }
+end
 
 return M
